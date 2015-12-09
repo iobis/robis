34918677e8563343372a9234e66d90c1bce00d1e
Samuel <samuel.bosch+git@gmail.com>
2015-12-09 15:59:59 +0100

    test aphiaid and obisid

--- R/obisclient.R
+++ R/obisclient.R
@@ -6,9 +6,6 @@
 #' @name obisclient
 NULL
 
-
-
-
 .url <- function(local) {
   # options(obisclient_url) <- "http://127.0.0.1:8090/"
   getOption("obisclient_url", "http://api.iobis.org/")
@@ -23,44 +20,47 @@ NULL
 #' @param startdate
 #' @param enddate
 #' @param geometry A wkt geometry string.
-#' @return The occurrences.
+#' @param qc A vector of qc numbers you want to filter out.
+#' @return The occurrence records.
 #' @examples
 #' occurrence(scientificname = "Abra sibogai")
+#' occurrence(scientificname = "Abra sibogai", qc = c(1:6, 27))
 #' @export
 occurrence <- function(
-  scientificname=NULL,
-  year=NULL,
-  obisid=NULL,
-  aphiaid=NULL,
-  startdate=NULL,
-  enddate=NULL,
-  geometry=NULL,
-  verbose=FALSE) {
+  scientificname = NULL,
+  year = NULL,
+  obisid = NULL,
+  aphiaid = NULL,
+  startdate = NULL,
+  enddate = NULL,
+  geometry = NULL,
+  qc = NULL,
+  verbose = FALSE) {
 
   baseurl <- paste0(.url(local), "occurrence?")
 
   if (!is.null(scientificname)) {
     baseurl <- paste0(baseurl, "&scientificname=", scientificname)
   }
-
   if (!is.null(year)) {
     baseurl <- paste0(baseurl, "&year=", year)
   }
-
   if (!is.null(obisid)) {
     baseurl <- paste0(baseurl, "&obisid=", obisid)
   }
-
   if (!is.null(aphiaid)) {
     baseurl <- paste0(baseurl, "&aphiaid=", aphiaid)
   }
-
   if (!is.null(startdate)) {
     baseurl <- paste0(baseurl, "&startdate=", startdate)
   }
-
   if (!is.null(geometry)) {
     baseurl <- paste0(baseurl, "&geometry=", geometry)
+  }
+  if(!is.null(qc)) {
+    qc <- setdiff(qc, 9) ## ignore QC 9 (NOT IMPLEMENTED)
+    qc <- qc[qc > 1 & qc <= 30] ## restrict to valid qcnumbers range
+    baseurl <- paste0(baseurl, paste0("&qc=", qc, collapse = ""))
   }
 
   offset <- 0
@@ -71,19 +71,21 @@ occurrence <- function(
 
   while (!lastpage) {
     url <- paste0(baseurl, "&offset=", format(offset, scientific=FALSE))
-
+    if (verbose) {
+      cat(url, "\n")
+    }
     result <- httr::GET(URLencode(url), httr::user_agent("obisclient - https://github.com/iobis/obisclient"))
     httr::stop_for_status(result)
     res <- httr::content(result, simplifyVector=TRUE)
+
+    ## TODO print message
 
     limit <- res$limit
     offset <- offset + limit
     lastpage <- res$lastpage
     datalist[[i]] <- res$results
     total <- total + nrow(res$results)
-    if (verbose) {
-      cat(url, "\n")
-    }
+
     cat("\rRetrieved ", total, " records of ", res$count, " (", floor(total/res$count*100),"%)", sep="")
     i <- i + 1
   }
@@ -91,3 +93,4 @@ occurrence <- function(
   data <- rbind.fill(datalist)
   return(data)
 }
+
--- tests/testthat/test_occurrence.R
+++ tests/testthat/test_occurrence.R
@@ -1,8 +1,42 @@
 library(obisclient)
 context("occurrence")
 
-test_that("occurrence returns some records for Abra sibogai", {
-  records <- occurrence(scientificname = "Abra sibogai")
+small_test_species <- "Abra sibogai"
+small_test_aphiaid <- 345684
+
+test_that("occurrence returns small number of records for a scientific name", {
+  records <- occurrence(scientificname = small_test_species)
   expect_more_than(nrow(records), 0)
   expect_true(is.data.frame(records))
+  expect_true(all(records$scientificName == small_test_species))
+  expect_true(all(records$aphiaID == small_test_aphiaid))
+})
+
+test_that("occurrence returns small number of records for an aphia id", {
+  records <- occurrence(aphiaid = small_test_aphiaid)
+  expect_more_than(nrow(records), 0)
+  expect_true(is.data.frame(records))
+  expect_true(all(records$aphiaID == small_test_aphiaid))
+})
+
+test_that("occurrence returns small number of records for an obis id", {
+  records <- occurrence(aphiaid = small_test_aphiaid) ## obis ids are not stable so taking a detour
+  records <- occurrence(obisid = records$obisID[1])
+  expect_more_than(nrow(records), 0)
+  expect_true(is.data.frame(records))
+  expect_true(all(records$taxonID == small_test_obisid))
+  expect_true(all(records$aphiaID == small_test_aphiaid))
+})
+
+test_that("qc flags in queries are respected", {
+  records <- occurrence(scientificname = small_test_species)
+
+  check_qc_no_zero <- function(records) {
+    qc_ok <- sapply(1:30, function(qc) { c(row=bitwAnd(records$qc, 2^(qc-1)) > 0) })
+    which(colSums(qc_ok) < nrow(qc_ok) & colSums(qc_ok) != 0)
+  }
+  qc_numbers_not_ok <- check_qc_no_zero(records)
+  records_with_qc <- occurrence(scientificname = "Abra sibogai", qc=qc_numbers_not_ok)
+  expect_equal(length(check_qc_no_zero(records_with_qc)), 0)
+  expect_less_than(nrow(records_with_qc), nrow(records))
 })
