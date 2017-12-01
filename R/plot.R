3c5660d2e6e1366b50449774fc1c01362b16459e
Pieter Provoost <pieterprovoost@gmail.com>
2017-12-01 11:52:31 +0100

    optional id parameter

--- R/plot.R
+++ R/plot.R
@@ -1,11 +1,16 @@
 #' Create a Leaflet map.
 #'
 #' @param data
+#' @param popup Column to be used for popups.
 #' @return HTML widget object.
 #' @export
-leafletmap <- function(data) {
+leafletmap <- function(data, popup = NULL) {
   m <- leaflet(data) %>%
-    addProviderTiles("OpenStreetMap.BlackAndWhite") %>%
-    addCircleMarkers(~decimalLongitude, ~decimalLatitude, popup = ~as.character(id), radius = 3, weight = 1, fillColor = "red", color = "red", opacity = 1, fillOpacity = 0.1)
+    addProviderTiles("OpenStreetMap.BlackAndWhite")
+  p <- NULL
+  if (!is.null(popup)) {
+    p <- as.character(data[,popup])
+  }
+  m <- m %>% addCircleMarkers(~decimalLongitude, ~decimalLatitude, popup = p, radius = 3, weight = 1, fillColor = "red", color = "red", opacity = 1, fillOpacity = 0.1)
   return(m)
 }
