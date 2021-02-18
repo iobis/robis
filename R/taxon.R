f400e57d1cf97ccce8cfd3273aef570447cf1469
Pieter Provoost <pieterprovoost@gmail.com>
2021-02-18 14:00:51 +0100

    fix for large taxon requests

--- R/taxon.R
+++ R/taxon.R
@@ -11,12 +11,21 @@ taxon <- function(
   taxonid,
   verbose = FALSE
 ) {
-  result <- http_request("GET", paste0("taxon/", handle_vector(taxonid)), list())
-  if (verbose) {
-    log_request(result)
+  if (is.character(taxonid)) {
+    taxonid <- str_trim(strsplit(taxonid, ",")[[1]])
   }
-  stop_for_status(result)
-  text <- content(result, "text", encoding = "UTF-8")
-  res <- fromJSON(text, simplifyVector = TRUE)
-  return(as_tibble(res$results))
+  ids <- split(taxonid, ceiling(seq_along(taxonid) / 100))
+  results <- purrr::map(ids, function(ids) {
+    ids <- handle_vector(ids)
+    result <- http_request("GET", paste0("taxon/", ids), list())
+    if (verbose) {
+      log_request(result)
+    }
+    stop_for_status(result)
+    text <- content(result, "text", encoding = "UTF-8")
+    res <- fromJSON(text, simplifyVector = TRUE)
+    res$results
+  })
+  names(results) <- NULL
+  return(as_tibble(bind_rows(results)))
 }
