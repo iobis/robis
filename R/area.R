a956a848eaa70fdd452bbfaf89487de73db69cf7
Pieter Provoost <pieterprovoost@gmail.com>
2021-07-05 12:57:04 +0200

    area fix

--- R/area.R
+++ R/area.R
@@ -6,7 +6,7 @@
 #' @examples
 #' areas <- area(verbose = FALSE)
 #' @export
-area <- function() {
+area <- function(verbose = FALSE) {
   result <- http_request("GET", "area", NULL, verbose)
   if (is.null(result)) return(invisible(NULL))
   text <- content(result, "text", encoding = "UTF-8")
