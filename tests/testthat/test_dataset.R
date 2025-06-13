fecaf14401965c8776b4358f4e996ed16d0c0c6b
sformel <steve@formeldataservices.com>
2025-06-13 11:18:36 -0500

    Update dataset keyword search to /search2 endpoint and support symbolic operators, validation, and documentation

--- tests/testthat/test_dataset.R
+++ tests/testthat/test_dataset.R
@@ -1,20 +1,50 @@
-test_that("dataset() basic call works", {
-  res <- dataset()
-  expect_s3_class(res, "tbl_df")
-})
+library(testthat)
 
-test_that("dataset() with scientificname works", {
+test_that("Scientific name search returns results", {
   res <- dataset(scientificname = "Tellinidae")
   expect_s3_class(res, "tbl_df")
+  expect_gt(nrow(res), 0)
 })
 
-test_that("dataset() keyword search works", {
-  res <- dataset(keyword = "coral reef")
+test_that("Area ID filter returns results", {
+  res <- dataset(areaid = 10181)
+  expect_s3_class(res, "tbl_df")
+  expect_gt(nrow(res), 0)
+})
+
+test_that("Keyword-only search returns results", {
+  res <- dataset(keyword = "coral")
+  expect_s3_class(res, "tbl_df")
+  expect_gt(nrow(res), 0)
+})
+
+test_that("Keyword with logical syntax works", {
+  res <- dataset(keyword = '"Gulf of Mexico" OR sponge -juvenile')
   expect_s3_class(res, "tbl_df")
 })
 
-test_that("dataset() keyword search returns some results", {
-  res <- dataset(keyword = "deep sea")
+test_that("Keyword and structured filters trigger error", {
+  expect_error(
+    dataset(keyword = "coral", scientificname = "Tellinidae"),
+    regexp = "no other filter parameters may be specified"
+  )
+})
+
+test_that("Invalid keyword returns empty result safely", {
+  res <- dataset(keyword = "NOT_A_REAL_TERM_123456")
   expect_s3_class(res, "tbl_df")
-  expect_true(nrow(res) >= 0)  # Can be 0 or more
+  expect_equal(nrow(res), 0)
+})
+
+test_that("validate_keyword detects unbalanced quotes", {
+  expect_warning(validate_keyword('"coral reef'), "Unbalanced")
+})
+
+test_that("validate_keyword warns on unsupported wildcards", {
+  expect_warning(validate_keyword("*star*"), "Double-sided wildcards")
+})
+
+test_that("Grouped OR query returns results", {
+  res <- dataset(keyword = "(coral | sponge)")
+  expect_s3_class(res, "tbl_df")
 })
